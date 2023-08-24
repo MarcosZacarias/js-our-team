8154abbd214da1978c8b6281fb9e5dfdeea31e6d
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-08-24 14:58:41 +0200

    add: array members list and img

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// |Creo una lista dei membri del Team
+const membersTeam = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    photo: "wayne-barnett-founder-ceo.jpg",
+  },
+
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    photo: "angela-caroll-chief-editor.jpg",
+  },
+
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    photo: "walter-gordon-office-manager.jpg",
+  },
+
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    photo: "angela-lopez-social-media-manager.jpg",
+  },
+
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    photo: "scott-estrada-developer.jpg",
+  },
+
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    photo: "barbara-ramos-graphic-designer.jpg",
+  },
+];
+
+// |Inserisco nella lista _membri team_ i membri
+
+// Stampo nome, ruolo e foto di ogni membro
+
+// - per ogni membro in _membri team_ stampa il il valore dentro le parole chiavi
