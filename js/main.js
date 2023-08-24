97a6103b108da08c5cf6724ec1fcd9bc101a1563
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-08-24 15:38:54 +0200

    add: layout cards

--- js/main.js
+++ js/main.js
@@ -53,14 +53,18 @@ const card = document.getElementById("card-container");
 // *Per ogni oggetto nell'array
 for (const member of membersTeam) {
   card.innerHTML += `
-  <div class="card mt-5" style="width: 18rem">
-    <img src= ./img/${member.photo} class="card-img-top" alt="..." />
-    <div class="card-body">
-      <h5 class="card-title">${member.name}</h5>
-      <p class="card-text">
-      ${member.role}                 
-      </p>
-    </div>
+  <div class="col-4">
+  <div class="d-flex justify-content-center">
+  <div class="card mt-3" style="width: 18rem">
+  <img src= ./img/${member.photo} class="card-img-top" alt="..." />
+  <div class="card-body">
+  <h5 class="card-title">${member.name}</h5>
+  <p class="card-text">
+  ${member.role}                 
+  </p>
+  </div>
+  </div>
+  </div>
   </div>
 `;
 }
